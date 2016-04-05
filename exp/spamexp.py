35744623316f63c911dca8f151fef8a9faebd323
Charanpal <charanpal@gmail.com>
2016-04-05 17:03:56 +0100

    Compress a bit

--- exp/spamexp.py
+++ exp/spamexp.py
@@ -1,27 +1,16 @@
 import pandas
-from sklearn.cross_validation import cross_val_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
+import sklearn, sklearn.feature_extraction, sklearn.svm
 
-input_filename = "../data/spam/SMSSpamCollection"
-input_file = open(input_filename)
+df = pandas.read_csv("../data/spam/SMSSpamCollection", sep="\t", header=-1)
 
-labels = []
-messages = []
+tf_transformer = sklearn.feature_extraction.text.TfidfVectorizer()
+X = tf_transformer.fit_transform(df[1])
+y = pandas.get_dummies(df[0], drop_first=True).values.ravel()
 
-for line in input_file.readlines():
-    words = line.split("\t")
-    labels.append(words[0])
-    messages.append(words[1])
-
-tf_transformer = TfidfVectorizer()
-X = tf_transformer.fit_transform(messages)
-y = pandas.get_dummies(labels, drop_first=True).values.ravel()
-
-learner = LinearSVC()
-scores = cross_val_score(learner, X, y, cv=5, scoring="accuracy")
+learner = sklearn.svm.LinearSVC()
+scores = sklearn.cross_validation.cross_val_score(learner, X, y, cv=5, scoring="accuracy")
 print(scores.mean())
-scores = cross_val_score(learner, X, y, cv=5, scoring="recall")
+scores = sklearn.cross_validation.cross_val_score(learner, X, y, cv=5, scoring="recall")
 print(scores.mean())
-scores = cross_val_score(learner, X, y, cv=5, scoring="precision")
+scores = sklearn.cross_validation.cross_val_score(learner, X, y, cv=5, scoring="precision")
 print(scores.mean())
